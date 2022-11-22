90bd62c8ee19e643480fc4958219599dc3892a13
Tarikul-Tuhin <tituhinkh@gmail.com>
2022-11-22 22:38:59 +0600

    tuhin

---
+++ lib/data/core/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String BASE_URL = "https://api.themoviedb.org/3/";
+  static const String API_KEY = "a846caa5d5ece83f6c046a3a2beeabc9";
+  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+}
